c07ce7c66f499c28620cdcda0d1b05ac84e5cb37
Weston Triemstra <weston@netsign.com>
2014-12-08 14:52:53 -0800

    move test to proper place (again)

--- spec/lib/mandrill_event_spec.rb
+++ spec/lib/mandrill_event_spec.rb
@@ -1,5 +1,9 @@
 describe MandrillEvent do
 
+  let(:events) { [] }
+  let(:subscriber) { ->(evt){ events << evt } }
+  let(:reject_event) { double('reject') }
+
   describe '.configure' do
 
     it 'yields itself to the block' do
@@ -8,9 +12,28 @@ describe MandrillEvent do
       expect(yielded).to eq MandrillEvent
     end
 
-    it "requires a block argument" do
+    it 'requires a block argument' do
       expect { MandrillEvent.configure }.to raise_error ArgumentError
     end
 
   end
+
+  describe 'subscribing to a specific event type' do
+
+    before do
+      allow(reject_event).to receive(:[]).with('event').and_return('reject')
+    end
+
+    context 'with a subscriber that responds to #call' do
+      it 'calls the subscriber with the event' do
+        MandrillEvent.subscribe('reject', subscriber)
+
+        MandrillEvent.instrument(reject_event)
+
+        expect(events).to eq [reject_event]
+      end
+    end
+
+  end
+
 end
